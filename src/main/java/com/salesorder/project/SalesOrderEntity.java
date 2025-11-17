96a445500b21ed8c4659abf896ea4d66cea870da
Rohan@Jaiswal <jaiswalrohan1801@gmail.com>
2025-11-17 22:56:35 +0530

    Added HelloService class

---
+++ src/main/java/com/salesorder/project/SalesOrderEntity.java
@@ -0,0 +1,17 @@
+package com.salesorder.project;
+
+
+public class SalesOrderEntity {
+
+	void dispaly()
+	{
+		System.out.println("Sales Order Entity");
+	}
+	public static void main(String[] args) {
+		
+		SalesOrderEntity soe = new SalesOrderEntity();
+		soe.dispaly();
+		
+	}
+
+}
